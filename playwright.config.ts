b3ac786364151665c897498384b0a744e39bae6f
testervippro <92687973+testervippro@users.noreply.github.com>
2025-09-10 23:04:34 +0700

    Refactor Playwright config for better organization

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig, devices } from "@playwright/test";
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import { globalCache } from '@vitalets/global-cache';
+import { ProjectConfig } from './ProjectConfig';
 
 /**
  * Read environment variables from file.
@@ -9,87 +12,49 @@ import { defineConfig, devices } from "@playwright/test";
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
- * See https://playwright.dev/docs/test-configuration.
+ * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
-  testDir: "./tests",
-  testMatch: ["**/*.spec.ts", "**/*.test.ts"],
+const config: ProjectConfig = {
+   globalSetup: globalCache.setup, 
+    globalTeardown: globalCache.teardown,
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : "html",
-
-  //globalSetup: require.resolve('./global-setup'),
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    //baseURL: "http://localhost:3000/",
-    //storageState: "state.json",
+    trace: 'on-first-retry',
+    baseURLThoaikx: 'http://localhost:3000',
+
+    headloss: false,
   },
 
   /* Configure projects for major browsers */
   projects: [
-    // run by order declare in the array
     {
-      name: "setup db",
-      testMatch: /global\.setup\.ts/,
-    },
-    {
-      name: "cleanup db",
-      testMatch: /global\.teardown\.ts/,
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
 
-    /* Test against branded browsers. */
-    // https://dev.to/muhendiskedibey/how-to-full-screen-a-browser-in-playwright-1np1
     {
-      name: "chrome",
-      use: {
-        ...devices["Desktop Chrome"],
-        channel: "chrome",
-        screenshot: "only-on-failure",
-        viewport: null,
-        deviceScaleFactor: undefined,
-        // headless = true on CI, false locally
-        //// This always returns a boolean value
-        //https://stackoverflow.com/questions/784929/what-does-the-double-exclamation-mark-operator-do-in-javascript
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
-        headless: !!process.env.CI,
-        launchOptions: {
-          args: ["--start-maximized"],
-        },
-      },
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
 
     // {
-    //   name: "Microsoft Edge",
-    //   use: {
-    //     ...devices["Desktop Edge"],
-    //     channel: "msedge",
-    //     viewport: null,
-    //     deviceScaleFactor: undefined,
-    //     launchOptions: {
-    //       args: ["--start-maximized"],
-    //     },
-    //   },
-    // }, // or "msedge-beta" or 'msedge-dev'
-
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
     // },
 
     /* Test against mobile viewports. */
@@ -101,12 +66,25 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+};
+
+export default config;
+
